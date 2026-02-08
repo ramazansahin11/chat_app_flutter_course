810e16a0ad5356f212812d008a11c524e99c9826
Ramazan Sahin <ramazansahin968@gmail.com>
2026-02-09 00:03:25 +0300

    first commit!!

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(child: SingleChildScrollView(child: Column())),
+    );
+  }
+}
